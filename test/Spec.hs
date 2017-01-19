16701afa7a274bba715b41db132b3d4456bdb6e9
Joseph Abrahamson <me@jspha.com>
2017-01-19 01:58:56 -0500

    Add (null) specs to begin test cycles

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,5 +3,10 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Testing framework" $ do
+    it "runs" $ do
+      True `shouldBe` True
